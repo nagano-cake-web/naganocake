de751be69bae659670e945b722077280abcd0547
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-09-15 16:02:17 +0000

    publicのroutes記述

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,15 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  namespace :public do
+
     get '/' => 'homes#top'
     get '/about'
-  end
+    resources :items, only: [:index, :show]
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :comfirm, :complete, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
