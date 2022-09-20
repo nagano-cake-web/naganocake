271ddff86253757c2d9c0a33a807c21ef82d9960
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-09-20 17:59:31 +0000

    マイページ作成中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,18 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def customer_paramd
+    params.require(:customer).permit(:name,)
 end
