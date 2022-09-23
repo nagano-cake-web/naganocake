670f1ff4ff86409440a8b8fae785297ab594d4b1
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-09-23 18:09:20 +0000

    item controller create記述中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -9,10 +9,18 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  #未完成
   def create
     @item = Item.new(item_params)
-    item.save
-    redirect_to items_path
+    if Item.find_by(name: params[:id])
+      item += params[:item][:amout].to_i
+      item.save
+      redirect_to items_path
+    elsif @item.save
+      redirect_to items_path
+    else
+      redirect_to items_path
+    end
   end
 
 end
