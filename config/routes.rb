c8f72e74077106430154de2e9d4e3c307f1ebba9
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-09-28 17:36:50 +0000

    routes編集完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
-}
+  }
 
   namespace :admin do
     get '' => 'homes#top'
@@ -23,13 +23,29 @@ Rails.application.routes.draw do
     get '/about' => 'public/homes#about'
     get '/items' => 'public/items#index'
     get '/items/:id' => 'public/items#show', as: 'item_show'
-    get '/customers' => 'public/customers#show'
-    get '/customers/information/edit' => 'public/customers#edit'
-    get '/customers/information' => 'public/customers#update'
-    get '/customers/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
-    get '/customers/withdraw' => 'users#withdraw', as: 'withdraw'
-    resources :cart_items, only: [:index, :update, :destroy, :create]
-    resources :orders, only: [:new, :create, :index, :show]
-    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+    get '/customers' => 'public/customers#show', as: 'customer_show'
+    get '/customers/information/edit' => 'public/customers#edit', as: 'customer_edit'
+    patch '/customers/information' => 'public/customers#update'
+    get '/customers/unsubscribe' => 'public#unsubscribe', as: 'unsubscribe'
+    patch '/customers/withdraw' => 'public#withdraw', as: 'withdraw'
+
+    get '/cart_items' => 'public/cart_items#index'
+    patch '/cart_items/:id' => 'public/cart_items#update', as: 'cart_item_update'
+    delete '/cart_items/:id' => 'public/cart_items#destroy', as: 'cart_item_destroy'
+    delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all'
+    post '/cart_items' => 'public/cart_items#create', as: 'cart_item_create'
+
+    get '/orders/new' => 'public/orders#new'
+    post '/orders/comfirm' => 'public/orders#comfirm'
+    post '/orders' => 'public/orders#create', as: 'order_create'
+    get '/orders' => 'public/orders#index'
+    get '/orders/:id' => 'public/orders#show', as: 'order_show'
+
+    get '/addresses' => 'public/addresses#index'
+    get '/addresses/:id/edit' => 'public/addresses#edit', as: 'address_edit'
+    post '/addresses' => 'public/addresses#create', as: 'address_create'
+    patch '/addresses/:id' => 'public/addresses#update', as: 'address_update'
+    delete '/addresses/:id' => 'public/addresses#destroy', as: 'address_destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
