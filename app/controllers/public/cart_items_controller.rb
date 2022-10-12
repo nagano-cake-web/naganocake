cfacab4ff5a9ab6cd8fa639931d8069f3fc0fbfb
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-12 19:22:24 +0000

    レイアウト変更中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,17 +2,18 @@ class Public::CartItemsController < ApplicationController
   def index
     @total = 0
     @cart_items = current_customer.cart_items
-
+    @item = Item.all
   end
 
-  def updete
+  def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
     redirect_to cart_items_path(cart_item.id)
   end
 
   def destroy_all
-    CartItem.destroy_all
+    cart_items = CartItem.all
+    cart_items.destroy_all
     redirect_to cart_items_path(current_customer.id)
   end
 
@@ -27,20 +28,21 @@ class Public::CartItemsController < ApplicationController
   def create
      @cart_item = CartItem.new(cart_item_params)
      @cart_item.customer_id = current_customer.id
-    if Item.find_by(name: params[:id])
-      cart_item += params[:item][:amout].to_i
-      cart_item.save
-      redirect_to items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      new_amount = cart_item.amount + params[:cart_item][:amount].to_i
+      cart_item.update!(amount: new_amount)
+      redirect_to cart_items_path
     elsif @cart_item.save
-      redirect_to items_path
+      redirect_to cart_items_path
     else
-      redirect_to items_path
+      redirect_to cart_items_path
     end
   end
 
   private
 
-  #編集などうまくいかない場合、permitにカラムを追加
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -9,24 +9,11 @@ class Public::ItemsController < ApplicationController
   def show
     @genre = Genre.all
     @item = Item.find(params[:id])
-    @customer = Customer.find(current_customer.id)
+    @customer = Customer.find(params[:id])
     @cart_item = CartItem.new
   end
 
-  #未完成
-  def create
-    @item = Item.new(item_params)
-    if Item.find_by(name: params[:id])
-      b
-      item += params[:item][:amout].to_i
-      item.save
-      redirect_to items_path
-    elsif @item.save
-      redirect_to items_path
-    else
-      redirect_to items_path
-    end
-  end
+
 
   private
 
