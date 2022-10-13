a065481df0ab67b4532f29e9681b27f04e78c801
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-13 18:16:13 +0000

    注文機能作成中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,14 +8,41 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def create
-    cart_items = current_customer.cart_items.all
+  def show
+    @order_detail = OrderDetail.all
+    @order = Order.all
+  end
+
+  def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:address_number] == "1"
+      @order.name = current_customer.name
+      @order.address = current_customer.address
+    elsif params[:order][:address_number] == "2"
+      if Address.exists?(name: params[:order][:address_id])
+        @order.name = Address.find(params[:order][:address_id]).name
+        @order.address = Address.find(params[:order][:address_id]).address
+      else
+        render :new
+      end
+    elsif  params[:order][:address_number] == "3"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+      else
+        render :new
+      end
+    @cart_items = current_customer.cart_items.all
+    end
+  end
+
+  def complete
+    @cart_items = current_customer.cart_items.all
     # ログインユーザーのカートアイテムをすべて取り出して cart_items に入れます
-    @order = current_customer.order.new(order_params)
+    @order = current_customer.orders.new(order_params)
     # 渡ってきた値を @order に入れます
     if @order.save
     # ここに至るまでの間にチェックは済ませていますが、念の為IF文で分岐させています
-      cart_items.each do |cart|
+      @cart_items.each do |cart|
         order_detail = OrderDatail.new
         order_detail.item_id = cart.item_id
         order_detail.order_id = @order.id
@@ -25,41 +52,9 @@ class Public::OrdersController < ApplicationController
     # カート情報を削除するので item との紐付けが切れる前に保存します
         order_detail.save
       end
-      redirect_to order_comfirm_path
+      redirect_to orders_complete_path
       cart_items.destroy_all
       # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
-    else
-      @order = Order.new(order_params)
-      render :new
-    end
-  end
-
-  def show
-    @order_detail = OrderDetail.all
-    @order = Order.all
-  end
-
-  def comfirm
-    @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
-  end
-
-  def complete
-    cart_items = current_customer.cart_items.all
-    @order = current_customer.orders.new(order_params)
-    if @order.save
-      cart_items.each do |cart|
-        order_item = OrderItem.new
-        order_item = @order.id
-        order_item.amount = cart.amount
-        order_item.total_payment = cart.item.price
-        order_item.save
-      end
-      redirect_to orders_comfirm_path
-      cart_items.destroy_all
     else
       @order = Order.new(order_params)
       render :new
@@ -69,7 +64,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:customers_id, :postal_code, :address, :name,
+    params.require(:order).permit(:customers_id, :address_id, :address_number,:postal_code, :address, :name,
                                   :shipping_cost,:total_payment, :payment_method, :status)
   end
 end
