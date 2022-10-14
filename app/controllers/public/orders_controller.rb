4c815253247194ad05957ceb6665cf33b965e500
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-14 18:29:49 +0000

    注文画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,6 +14,26 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
+    @cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+      @cart_items.each do |cart|
+        order_detail = OrderDatail.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.order_amount = cart.amount
+        order_detail.order_price = cart.item.price
+        order_detail.save
+      end
+      redirect_to orders_complete_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+  end
+
+  def complete
     @order = Order.new(order_params)
     if params[:order][:address_number] == "1"
       @order.name = current_customer.name
@@ -35,36 +55,10 @@ class Public::OrdersController < ApplicationController
     end
   end
 
-  def complete
-    @cart_items = current_customer.cart_items.all
-    # ログインユーザーのカートアイテムをすべて取り出して cart_items に入れます
-    @order = current_customer.orders.new(order_params)
-    # 渡ってきた値を @order に入れます
-    if @order.save
-    # ここに至るまでの間にチェックは済ませていますが、念の為IF文で分岐させています
-      @cart_items.each do |cart|
-        order_detail = OrderDatail.new
-        order_detail.item_id = cart.item_id
-        order_detail.order_id = @order.id
-        order_detail.order_amount = cart.amount
-    # 購入が完了したらカート情報は削除するのでこちらに保存します
-        order_detail.order_price = cart.item.price
-    # カート情報を削除するので item との紐付けが切れる前に保存します
-        order_detail.save
-      end
-      redirect_to orders_complete_path
-      cart_items.destroy_all
-      # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
-    else
-      @order = Order.new(order_params)
-      render :new
-    end
-  end
-
   private
 
   def order_params
-    params.require(:order).permit(:customers_id, :address_id, :address_number,:postal_code, :address, :name,
+    params.require(:order).permit(:customers_id, :address_number,:postal_code, :address, :name,
                                   :shipping_cost,:total_payment, :payment_method, :status)
   end
 end
