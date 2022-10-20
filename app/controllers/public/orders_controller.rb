bad4b6785c5d3ea74731fb316572f2631644c8c9
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-20 09:46:04 +0000

    注文画面編集中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -16,18 +16,17 @@ class Public::OrdersController < ApplicationController
   def create
     @cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
-    @order.shipping_cost = 800
     if @order.save
       @cart_items.each do |cart|
-        order_detail = OrderDatail.new
+        order_detail = OrderDetail.new
         order_detail.item_id = cart.item_id
         order_detail.order_id = @order.id
-        order_detail.order_amount = cart.amount
-        order_detail.order_price = cart.item.price
+        order_detail.amount = cart.amount
+        order_detail.price = @order.total_payment
         order_detail.save
       end
       redirect_to orders_complete_path
-      cart_items.destroy_all
+      @cart_items.destroy_all
     else
       @order = Order.new(order_params)
       render :new
@@ -46,8 +45,8 @@ class Public::OrdersController < ApplicationController
     elsif params[:order][:select_address] == "2"
       @address = Address.find(params[:order][:address_id])
       if Address.exists?(name: params[:order][:address_id])
-        @order.name = Address.find(params[:order][:address_id]).name
-        @order.address = Address.find(params[:order][:address_id]).address
+        @order.name = @address.name
+        @order.address = @address.address
       else
         render :new
       end
@@ -68,6 +67,6 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:customers_id, :postal_code, :address, :name,
-                                  :shipping_cost,:total_payment, :payment_method, :status)
+                                  :shipping_cost, :total_payment, :payment_method)
   end
 end
