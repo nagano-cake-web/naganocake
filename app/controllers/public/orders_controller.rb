62d8af022c03d296fb5ca40cd51711314ae43fe5
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-20 12:59:21 +0000

    注文機能登録住所選択完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -37,16 +37,15 @@ class Public::OrdersController < ApplicationController
      @order = Order.new(order_params)
      @cart_items = current_customer.cart_items
      @order.shipping_cost = 800
-     @address = Address.find(params[:order][:address_id])
     if params[:order][:select_address] == "1"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:select_address] == "2"
-      @address = Address.find(params[:order][:address_id])
-      if Address.exists?(name: params[:order][:address_id])
-        @order.name = @address.name
+      if @address = Address.find(params[:order][:address_id])
+        @order.postal_code = @address.postal_code
         @order.address = @address.address
+        @order.name = @address.name
       else
         render :new
       end
