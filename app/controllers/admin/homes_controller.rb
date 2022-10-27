46170818c4e45ed1f954fa23ba9d211667a849b3
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-10-27 15:36:33 +0000

    テスト中

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,13 @@
 class Admin::HomesController < ApplicationController
   def top
     @orders = Order.all
+    @order_details = OrderDetail.all
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order.id)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = Order.find(paramd[:id])
-    @orders = Order.all
-    @order_amount = Order
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
     @total = 0
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customers_id, :postal_code, :address, :name,
+                                  :shipping_cost, :total_payment, :payment_method)
   end
 end
